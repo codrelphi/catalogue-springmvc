e7194047690b490a8d8b6aa6e2b7ca5bfee81a0e
codrelphi <chancerel.codjovi@gmail.com>
2020-07-03 17:19:52 +0100

    Premier commit: fichiers générés par spring boot

---
+++ catalogue-springmvc/src/main/java/com/tlabs/CatalogueSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.tlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueSpringmvcApplication.class, args);
+	}
+
+}
---
+++ catalogue-springmvc/src/test/java/com/tlabs/CatalogueSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueSpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

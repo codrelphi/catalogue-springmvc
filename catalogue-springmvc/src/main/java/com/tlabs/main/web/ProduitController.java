4fd7035b146ef62cedd0d002bf4672de9ec1d061
codrelphi <chancerel.codjovi@gmail.com>
2020-07-04 12:50:03 +0100

    commit: pagination

--- catalogue-springmvc/src/main/java/com/tlabs/main/web/ProduitController.java
+++ catalogue-springmvc/src/main/java/com/tlabs/main/web/ProduitController.java
@@ -3,9 +3,12 @@ package com.tlabs.main.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tlabs.main.dao.ProduitRepository;
 import com.tlabs.main.entities.Produit;
@@ -17,9 +20,12 @@ public class ProduitController {
 	ProduitRepository produitRepository;
 	
 	@GetMapping(value= {"/", "/index"})
-	public String index(Model model) {
-		List<Produit> produits = produitRepository.findAll();
-		model.addAttribute("produits", produits);
+	public String index(Model model, @RequestParam(name="page", defaultValue="0") int page) {
+		//List<Produit> produits = produitRepository.findAll();
+		Page<Produit> pageProduits = produitRepository.findAll(PageRequest.of(page, 10));
+		model.addAttribute("produits", pageProduits.getContent());
+		model.addAttribute("pages", new int[pageProduits.getTotalPages()]);
+		model.addAttribute("currentPage", page);
 		return "produits";
 	}
 }
